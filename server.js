6f368555ed3daf0baf87a2871c114737fa1a6977
Shrey Joshi <shreyjoshi2004@gmail.com>
2020-07-26 23:32:02 -0500

    fix bugs and add more functionalit

--- server.js
+++ server.js
@@ -1,49 +1,80 @@
 const io = require("socket.io")();
+const listOfWords = ["Cat", "Dog", "Dance", "Baseball", "Happy", "Important", "Explosion"];
+// TODO LIST:
+// - Make all the data here and there into one big users object (ket is socket is and values is an object with more infomation)
 
-const users = {};
+
+
+let users = {};
 let usersReady = [];
 let roundIsRunning = false;
 let currentroundID = null;
 let roundTimeInterval = 10 * 1000;
 let gameHost = null;
-// Add round ID to make it so that leaving and then coming back and starting a new round doesnt make the old callback appear.
-function runRound(id) {
+const canvasBlankData = '{"lines":[{"points":[{"x":0,"y":0}, {"x":2000,"y":2000}],"brushColor":"#FFF","brushRadius":10000}],"width":"100%","height":"100%"}';
 
-    if (roundIsRunning) {
-        console.log("Started round.");
-        let drawer = usersReady[Math.floor(Math.random() * usersReady.length)];
-        io.emit("send-message", { sender: "Server", content: `Starting round... The first drawer is ${users[drawer]}` });
-        io.to(drawer).emit("send-message", { sender: "Server", content: "Your word is Cat." });
-        io.emit("drawer-change", users[drawer]);
-        // Give user some time to draw
-        setTimeout(() => {
-            if (roundIsRunning && id == currentroundID) {
-                io.emit("send-message", { sender: "Server", content: "Time's Up! The word was Cat." })
-                console.log("Ended game after successful round");
-                roundIsRunning = false;
-                usersReady = [];
-            } else {
-                console.log("ERROR: ROUND IDs DON'T MATCH");
-                console.log("Hoped to execute callback after timeout but round ended unexpectedly.");
-                console.log("Round running: " + roundIsRunning);
-                console.log("current/passed round id: " + currentroundID + " " + id);
-            }
-        }, roundTimeInterval)
 
-    }
+const welcomeMessage = "Hello and welcome to Scribblio! Please send '/ready' in the chat to let the server know you're ready to join a new game. On the left side there is a list of players with the color indicating their ready status.";
+const gameAlreadyStartedMessage = "Lol you joined a bit too late. The game already started. Refresh when the current game ends to join.";
+
+function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
 }
 
+function clearScreen() {
+    io.emit("canvas-change", { sender: "server", data: canvasBlankData });
+}
+// Add checker for all users ready or no
+
+function allUsersReady() {
+    // Sort and convert to strings since we cant compare arrays directly
+    const stringifiedUsers = JSON.stringify(Object.values(users).sort());
+    const stringifiedUsersThatAreReady = JSON.stringify(usersReady.sort());
+    console.log("users in game: " + stringifiedUsers);
+    console.log("users that are ready: " + stringifiedUsersThatAreReady);
+    if (stringifiedUsers === stringifiedUsersThatAreReady) {
+        return true;
+    }
+    return false;
+}
+
+function runRound(id, drawer, drawerID) {
+    console.log("Started round.");
+    clearScreen();
+    io.emit("send-message", { sender: "Server", content: `Starting round... The first drawer is ${drawer}` });
+    io.to(drawerID).emit("send-message", { sender: "Server", content: "Your word is Cat." });
+    io.emit("drawer-change", drawer);
+    // Give user some time to draw
+    setTimeout(() => {
+        // Check round ID if 
+        if (roundIsRunning && id == currentroundID) {
+            io.emit("send-message", { sender: "Server", content: "Time's Up! The word was Cat." })
+            console.log("Ended game after successful round");
+            roundIsRunning = false;
+            usersReady = [];
+            io.emit("ready-change", usersReady);
+        } else {
+            console.log("ERROR: ROUND IDs DON'T MATCH");
+            console.log("Hoped to execute callback after timeout but round ended unexpectedly.");
+            console.log("Round running: " + roundIsRunning);
+            console.log("current/passed round id: " + currentroundID + " " + id);
+        }
+    }, roundTimeInterval)
+
+}
 
 
 io.on("connect", (client) => {
     // If a round is running already disconnect the client
     if (roundIsRunning) {
-        client.emit("send-message", { sender: "Server", content: "Lol you joined a bit too late. The round already started. Refresh when the round ends" });
+        client.emit("send-message", { sender: "Server", content: gameAlreadyStartedMessage });
         client.disconnect();
     }
 
     // Send the newly connected client a list of connected ppl (not them tho bc they haven't sent username yet)
     client.emit("user-list", Object.values(users));
+    client.emit("new-host", gameHost);
+    client.emit("ready-change", usersReady);
 
     // ALl the other stuff happend once the client connects with their username
     client.on("send-username", userThatJoined => {
@@ -53,43 +84,65 @@ io.on("connect", (client) => {
         io.emit("send-message", { username: userThatJoined, content: joinMessage, type: "meta-join" });
         io.emit("user-list", Object.values(users));
         io.emit("new-host", gameHost);
+        client.emit("send-message", { sender: "Server", content: welcomeMessage });
 
 
-        // If this client is the first to connect make the client host
+        // If this client is the first to connect make the client host and drawer
         if (Object.keys(users).length === 1) {
-            gameHost = users[client.id]
+            gameHost = userThatJoined;
             io.emit("new-host", gameHost);
+            io.emit("drawer-change", userThatJoined);
         }
 
 
         client.on("send-message", msg => {
-            io.emit("send-message", { sender: users[client.id], content: msg });
-            io.emit("drawer-change", users[client.id]);
-            console.log(msg);
+            let nameOfSender = users[client.id];
+            console.log(nameOfSender + ": " + msg);
 
 
             // Logic to start game once everyone says "/ready"
             if (msg.toLowerCase() === "/ready") {
                 if (!roundIsRunning) {
-                    console.log("got ready message and round was not running.");
-                    if (!usersReady.includes(client.id)) {
-                        usersReady.push(client.id);
+                    // console.log("got ready message and round was not running.");
+                    if (!usersReady.includes(nameOfSender)) {
+                        usersReady.push(nameOfSender);
+                        // io.emit("send-message", { sender: "Server", content: `${nameOfSender} is ready!` });
+                        io.emit("ready-change", usersReady);
                     }
-                    // Sort and convert to strings since we cant compare arrays directly
-                    let usersInGame = JSON.stringify(Object.keys(users).sort());
-                    let usersThatAreReady = JSON.stringify(usersReady.sort());
-                    console.log("users in game: " + usersInGame);
-                    console.log("users that are ready: " + usersThatAreReady);
 
-                    if (usersInGame === usersThatAreReady && usersInGame !== 0) {
-                        roundIsRunning = true;
-                        console.log("starting game...");
-                        currentroundID = Math.random();
-                        runRound(currentroundID);
+                    if (allUsersReady()) {
+                        console.log("All users ready!");
+                        // THIS SHOULD BE A CONTIUOUS POPUP THING NOT A MESSAGE
+                        io.emit("send-message", { sender: "Server", content: `All users are ready! ${gameHost} needs to say '/start' to begin the game.` })
                     }
                 } else {
                     console.log("got ready message but round was already running.")
                 }
+            } else if (msg.toLowerCase() === "/unready") {
+                if (usersReady.includes(nameOfSender)) {
+                        usersReady = usersReady.filter(username => {
+                            return username !== nameOfSender;
+                        });
+                        // io.emit("send-message", { sender: "Server", content: `${nameOfSender} is ready!` });
+                        io.emit("ready-change", usersReady);
+                    }
+            } else if (msg.toLowerCase() === "/start") {
+                if (nameOfSender === gameHost) {
+                    if (allUsersReady()) {
+                        currentroundID = Math.random();
+                        roundIsRunning = true;
+                        let firstDrawerID = getRandomElement(Object.keys(users));
+                        let firstDrawerName = users[firstDrawerID]
+                        runRound(currentroundID, firstDrawerName, firstDrawerID);
+                    } else {
+                        client.emit("send-message", { sender: "Server", content: "Can't start. Not everyone's ready." });
+                    }
+                } else {
+                    client.emit("send-message", { sender: "Server", content: "You're not the host bro stop trying to start the game." });
+                }
+            } else {
+                io.emit("send-message", { sender: nameOfSender, content: msg });
+                io.emit("drawer-change", nameOfSender);
             }
 
 
@@ -98,24 +151,32 @@ io.on("connect", (client) => {
 
 
         client.on("disconnect", () => {
+            // clearScreen();
             const userThatLeft = users[client.id];
+            delete users[client.id];
 
-            if (roundIsRunning) {
-                usersReady = usersReady.filter(userid => {
-                    return users[userid] != userThatLeft
-                });
-                console.log(userThatLeft + " left during game. New users list: " + users[usersReady]);
+            usersReady = usersReady.filter(username => {
+                return username != userThatLeft;
+            });
+            console.log(userThatLeft + " left during game. New users list: " + users[usersReady]);
+
+            if (Object.keys(users).length === 0) {
+                users = {};
+                usersReady = [];
+                roundIsRunning = false;
+                currentroundID = null;
+                gameHost = null;
+                console.log("No users active. Ended game and reset all defaults.");
             }
 
-            delete users[client.id];
             const leaveMessage = `has left the server.`
             io.emit("send-message", { username: userThatLeft, content: leaveMessage, type: "meta-leave" });
             io.emit("user-list", Object.values(users));
 
-            if (Object.keys(users).length === 0 && roundIsRunning) {
-                roundIsRunning = false;
-                usersReady = [];
-                console.log("No users active. Ended game.")
+            if (gameHost === userThatLeft) {
+                let newHost = getRandomElement(usersReady)
+                io.emit("new-host", newHost);
+                io.emit("send-message", { sender: "Server", content: `The new host is ${newHost}` })
             }
         });
 
